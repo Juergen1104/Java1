ff30c8568d87e1533f867d9429d64be190f76667
jpohl <jpohl@web.de>
2022-10-09 22:54:32 +0200

    Aufgabe 3 next step

--- src/Michelle/Aufgabe_Sudoku/Sudoku.java
+++ src/Michelle/Aufgabe_Sudoku/Sudoku.java
@@ -11,31 +11,137 @@ public class Sudoku {
 
     /**
      * The main method, invoking printArray() and testSolution().
+     *
      * @param args Unused.
      */
     public static void main(String[] args) {
-        printArray(sudoku[1]);
+        printArray(sudoku);
         testSolution();
     }
 
     public static void testSolution() {
 
+        boolean rowsOK=testRows(sudoku);
+        boolean colsOK=testCols(sudoku);
+        boolean squaresOK=testSquares(sudoku);
+
+        if (rowsOK&&colsOK&&squaresOK){
+            System.out.println("alles richtig");
+        }else {
+            System.out.println("Fehlerhaft");
+        }
+
+
     }
 
-    public static void printArray(int [] werte)  {
+    public static boolean testRows(int[][] sudoku) {
 
-        String row="";
-        int i=0;
+        boolean fehlerfrei = true;
 
-        do{
+        int[][] copySudoku = new int[sudoku.length][sudoku[0].length];
+        for (int i = 0; i < sudoku.length; i++) {
+            for (int j = 0; j < sudoku[i].length; j++) {
+                copySudoku[i][j] = sudoku[i][j];
+            }
+            if (!check1To9(copySudoku[i])) {
+                System.out.println("Fehler in Zeile " + i + 1);
+                fehlerfrei = false;
+            }
+        }
+        return fehlerfrei;
+    }
 
-            row = row + werte[i];
+    public static boolean testCols(int[][] sudoku) {
+        boolean fehlerfrei = true;
+        int[][] copySudoku = new int[sudoku.length][sudoku[0].length];
+        int[] testrow = new int[sudoku.length];
 
+        for (int j = 0; j < sudoku.length; j++) {
+            for (int i = 0; i < sudoku[j].length; i++) {
+                copySudoku[i][j] = sudoku[i][j];
+                testrow[i] = sudoku[i][j];
+            }
+            if (!check1To9(testrow)) {
+                fehlerfrei = false;
+            }
+        }
+        return fehlerfrei;
+    }
+
+    public static boolean testSquares(int[][] sudoku) {
+        boolean fehlerfrei = true;
+        int[] testSquare = new int[9];
+        int m = 0;
+
+        for (int i = 0; i < 7; i = i + 3) {
+            for (int j = 0; j < 7; j = j + 3) {
+
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++){
+                        testSquare[m] = sudoku[i + k][j + l];
+                        m++;
+                    }
+                }
+                if (!check1To9(testSquare)) {
+                    fehlerfrei = false;}
+                m = 0;
+            }
+        }
+        return fehlerfrei;
+    }
+
+    public static boolean check1To9(int[] zahlen) {
+        int k = 0;
+        for (int i = zahlen.length - 1; i > 1; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+                if (zahlen[j] >= zahlen[i]) {
+                    k = zahlen[i];
+                    zahlen[i] = zahlen[j];
+                    zahlen[j] = k;
+                }
+            }
+        }
+        for (int i = 0; i < zahlen.length - 2; i++) {
+            if (zahlen[i] == zahlen[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printArray(int[][] sudokuAll) {
+
+        int i = 0;
+        int abstand = 1;
+
+        do {
+            printrow(sudokuAll[i]);
+            if (abstand == 3) {
+                System.out.println(" ");
+                abstand = 0;
+            }
             i++;
-
-
-        } while (werte.length<i);
-
+            abstand++;
+        } while (i < sudokuAll.length);
     }
+
+    public static void printrow(int[] werte) {
+
+        String row = "";
+        int i = 0;
+        int abstand = 1;
+
+        do {
+            row = row + werte[i] + " ";
+            if (abstand == 3) {
+                row = row + " ";
+                abstand = 0;
+            }
+            i++;
+            abstand++;
+        } while (i < werte.length);
+        System.out.println(row);
+    }
+
 
 }
