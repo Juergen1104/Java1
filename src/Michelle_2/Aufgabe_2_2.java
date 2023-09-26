3af695a636311c9d6841a2b14908e08da2dbd424
jpohl <jpohl@web.de>
2023-09-26 21:38:55 +0200

    Aufgaben 2.2 und 2.3

--- src/Michelle_2/Aufgabe_2_2.java
+++ src/Michelle_2/Aufgabe_2_2.java
@@ -1,16 +1,54 @@
 package Michelle_2;
 
+import Michelle_1.Aufgabe_2.IOTools;
+
+import static java.lang.String.valueOf;
+
 public class Aufgabe_2_2 {
 
-    public static void main(String[] args){
-
-        long zahl;
-
-        // test
+    public static void main(String[] args) {
 
 
-        
+        Long zahl = 0L;
+        String feld = "";
+        int geradeZiffern = 0;
+        int ungeradeZiffern = 0;
+
+        do {
+            zahl = IOTools.readLong("Bitte Zahl eingeben: ");
+
+            if (zahl < 10) {
+                feld = feld + valueOf(zahl);
+            }
+        }
+        while (zahl < 10);
+
+        zahl = Long.parseLong(feld);
+
+        System.out.println("Zahl = " + zahl);
+
+
+
+
+        while (zahl > 0) {
+
+            long letzteZiffer = zahl % 10; // Letzte Ziffer der Zahl extrahieren
+            if (letzteZiffer % 2 == 0) {
+                geradeZiffern++;
+            } else {
+                ungeradeZiffern++;
+            }
+            zahl /= 10;
+        }
+
+        System.out.println("Anzahl gerader Ziffern: " + geradeZiffern);
+        System.out.println("Anzahl ungerader Ziffern: " + ungeradeZiffern);
+
+
     }
-    
+
 }
+
+
+
 
--- src/Michelle_2/Aufgabe_2_3.java
+++ src/Michelle_2/Aufgabe_2_3.java
@@ -1,29 +1,72 @@
 package Michelle_2;
 
-public class Aufgabe_2_3{
+public class Aufgabe_2_3 {
     // Aufgabenteil (a) 
     // 0 1 2 3 6 11 20 37 68  ...
-    // Schema: 
+    // Schema:
+
+    // an = an-1+an-2+an-3
+
     public static void folge_a() {
-        
+        int[] intArray = new int[15];
+        // setze die ersten drei Werte
+        intArray[0] = 0;
+        intArray[1] = 1;
+        intArray[2] = 2;
+
+        for (int i = 3; i < 15; i++) {
+            intArray[i] = intArray[i - 1] + intArray[i - 2] + intArray[i - 3];
+        }
+
+        for (int i = 0; i < 15; i++) {
+            System.out.print(intArray[i] + " ");
+        }
     }
 
     // Aufgabenteil (b)
     // 1 6 4 9 7 12 10 15  ...
     // Schema:
+    // erste Zahl + 3 ergibt die dritte Zahl,
+    // zweite Zahl + 3 ergibt die vierte Zahl usw.
     public static void folge_b() {
-        
+
+        int[] intArray = new int[15];
+
+        intArray[0] = 1;
+        intArray[1] = 6;
+
+        for (int i = 2; i < 15; i++) {
+            intArray[i] = intArray[i - 2] + 3;
+        }
+        for (int i = 0; i < 15; i++) {
+            System.out.print(intArray[i] + " ");
+        }
+
+
     }
-    
+
     // Aufgabenteil (c)
     // 2 2 5 11 20 32 47 65  ...
     // Schema:
+    // 1 Zahl = 2
+    // zweite Zahl = 1 Zahl + 0 *3
+    // dritte Zahl = 2 Zahl + 1 * 3
+
     public static void folge_c() {
-        
+
+        int[] intArray = new int[15];
+
+        intArray[0] = 2;
+
+        for (int i = 1; i < 15; i++) {
+            intArray[i] = intArray[i - 1] + (i - 1) * 3;
+        }
+        for (int i = 0; i < 15; i++) {
+            System.out.print(intArray[i] + " ");
+        }
     }
 
-    
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Aufgabenteil (a)");
         folge_a();  // erste Folge berechnen und ausgeben
         System.out.println("\nAufgabenteil (b)");
