127b490f9050105d54b2bf238c609f3a4c2020dd
jpohl <jpohl@web.de>
2023-09-28 23:31:34 +0200

    final

--- src/Michelle_2/Aufgabe_2_1.java
+++ src/Michelle_2/Aufgabe_2_1.java
@@ -1,17 +1,21 @@
 package Michelle_2;
 
 public class Aufgabe_2_1 {
-    public static void main(String[] args){
-       
-	/* *** Aufgabenteil (a)  *** */
+    public static void main(String[] args) {
+
+        /* *** Aufgabenteil (a)  *** */
         // 1
 	/* 
-	    // TODO
+	    ist die Eingabe n < 0 wird n positiv gesetzt
+	    in der while schleife wird 1 subraiert
+	    dann nochmals -1
+	    bis n <= 0 ist
+	    erg1 ist true wenn es eine gerade zahl war und das Ergebnis = 0 ist
             
 	 */
-	int n = IOTools.readInteger("n = ");
+        int n = IOTools.readInteger("n = ");
 
-	boolean erg1;
+        boolean erg1;
 
         n = (n < 0) ? -n : n;
 
@@ -21,11 +25,11 @@ public class Aufgabe_2_1 {
         }
 
         erg1 = (n == 0);
-	
-	System.out.println("(a) Ergebnis:   = " + erg1 + "\n");
 
-        
- 	System.out.println("\n***********************\n");
+        System.out.println("(a) Ergebnis:   = " + erg1 + "\n");
+
+
+        System.out.println("\n***********************\n");
 
 
         /* *** Aufgabenteil (b)  *** */
@@ -33,80 +37,105 @@ public class Aufgabe_2_1 {
 	/* 
            // TODO
 
+           a und b werden eingegenben und in A und B gesichert
+
+                wenn a und b kleiner gleich null sind, dann wird
+                1 auf a und 1 auf b addiert solange a und b ungleich 0 sind
+                sind beide werte
+                wenn a = 0 dann wird A in erb2 übertragen, ansonsten B
+
+          ansonsten wenn a und b größer gleich null sind,
+                dann wird 1 von a und b subrtraiert
+                solange a und b ungleich 0 sind
+          wenn a gleich 0 ist, dann wird B in erg2 gespeichert, ansonsten A
+
+          ansonsten wemm a kleiner 0 ist, dann wird B in erg2 gespeichert, ansonsten A
+
         */
-        
+
         int a = IOTools.readInteger("a = ");
         int A = a;    // Originalwert von a merken
         int b = IOTools.readInteger("b = ");
         int B = b;    // Originalwert von b merken
-	int erg2;
+        int erg2;
 
-	if ((a <= 0) && (b <= 0)) {
+        if ((a <= 0) && (b <= 0)) {
             while ((a != 0) && (b != 0)) {
                 a++;
                 b++;
             }
             erg2 = (a == 0) ? A : B;
-        }
-        else if ((a >= 0) && (b >= 0)) {
+        } else if ((a >= 0) && (b >= 0)) {
             while ((a != 0) && (b != 0)) {
                 a--;
                 b--;
             }
             erg2 = (a == 0) ? B : A;
-        }
-        else if (a < 0) {
+        } else if (a < 0) {
             erg2 = B;
-        }
-        else {
+        } else {
             erg2 = A;
         }
 
-	System.out.println("(b) Ergebnis:   = " + erg2 + "\n");
+        System.out.println("(b) Ergebnis:   = " + erg2 + "\n");
 
- 	System.out.println("\n***********************\n");
+        System.out.println("\n***********************\n");
 
-        
-	/* *** Aufgabenteil (c)  *** */
+
+        /* *** Aufgabenteil (c)  *** */
         // 2
 	/* 
 	   // TODO
 
 	 */
 
-        /* Hier werden zunaechst zwei Werte a, b mit
-           a >= 0 und b >= 0 eingelesen
+        /*
+            /* Hier werden zunaechst zwei Werte a, b mit
+           a und b >= 0 eingelesen Zahl muss positiuv sein
+           x y und z initialisieren mit 0
+           die erste schleife läuft von 1 bis zum wert a in 1er Schritten
+           auf y wird b addiert und zwar a * Ergebnis von y = a * b
+           die zweite innere Schleife läuft von j = 1 bis j <= a in einer Schritten. x wird ebenfalls um 1 erhöht und hat am Ende der Schleife den Wert von a * a
+
+           die letzte Schleife läuft von i = 1 los bis i den Wert  von b erreicht
+           y wird um den wert a erhöht
+
+            die zweite innere Schleife läuft von j = 1 bis j <= b in einer Schritten. z wird ebenfalls um 1 erhöht und hat dan den Wert von b * b
+
+            Ergebnis: x = a*a, y = a*b + b*a, z=b*b
+            Ergebnis: a*a + (a*b + b*a) + b*b
          */
+
         do {
             a = IOTools.readInteger("a = ");
         } while (a <= 0);
         do {
             b = IOTools.readInteger("b = ");
-        } while (b <=0);
+        } while (b <= 0);
 
- 
-        
-	int x=0,y=0,z=0;
 
-	for (int i=1;i<=a;i++){
-	    y += b;
-	    for (int j=1;j<=a;j++,x++){}
-	}
+        int x = 0, y = 0, z = 0;
 
-	for (int i=1;i<=b;i++){
-	    y += a;
-	    for (int j=1;j<=b;j++,z++){}
-	}	
-	
-	int erg3 = x + y + z;
-	
-	System.out.println("(c) Ergebnis:   = " +  erg3);
+        for (int i = 1; i <= a; i++) {
+            y += b;
+            for (int j = 1; j <= a; j++, x++) {
+            }
+        }
 
- 
-        
- 	System.out.println("\n***********************\n");
+        for (int i = 1; i <= b; i++) {
+            y += a;
+            for (int j = 1; j <= b; j++, z++) {
+            }
+        }
 
- 
+        int erg3 = x + y + z;
+
+        System.out.println("(c) Ergebnis:   = " + erg3);
+
+
+        System.out.println("\n***********************\n");
+
+
     }
 }
 
--- src/Michelle_2/Aufgabe_2_4.java
+++ src/Michelle_2/Aufgabe_2_4.java
@@ -4,17 +4,43 @@ public class Aufgabe_2_4 {
     // 3P
     public static void teil_a() {
         // TODO (Aufgabenteil (a))
-        
+
+        int zielwert1 = 2023;
+        int zielwert2 = 2024;
+
+        for (int a = 1; a <= 1000; a++) {
+            for (int b = 1; b <= 1000; b++) {
+                for (int c = 1; c <= 1000; c++) {
+                    if (a * b + c == zielwert1 && a + b * c == zielwert2) {
+                        System.out.println("a = " + a + "  b = " + b + "  c = " + c);
+                    }
+                }
+            }
+        }
     }
 
     // 3P
     public static void teil_b() {
         // TODO (Aufgabenteil (b))
-        
+        // Formel x*x + y*y + z*z = 3 * x * y * z
+
+        int max = 500;
+        for (int x = 1; x <= max; x++) {
+            for (int y = x; y < max; y++) {
+                for (int z = y; z <= max; z++) {
+                    int ergebnis1 = x * x + y * y + z * z;
+                    int ergebnis2 = 3 * x * y * z;
+                    if (ergebnis1 == ergebnis2) {
+                        System.out.println("(" + x + ", " + y + ", " + z + ")");
+                    }
+
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
         teil_a();
-        teil_b();       
+        teil_b();
     }
 }
