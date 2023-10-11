68346e609472213c28aa07dfe5aadfbd27ff8719
jpohl <jpohl@web.de>
2023-10-11 20:44:32 +0200

    Aufgabe 4 Aufgabe 1

--- src/Michelle_4/Klassenmethoden.java
+++ src/Michelle_4/Klassenmethoden.java
@@ -2,16 +2,43 @@ package Michelle_4;
 
 public class Klassenmethoden {
 
-
     /* Aufgabenteil (a) */
     // Bestimmt die Anzahl der Ziffern einer Zahl
     // n > 0
     // 2
+    public static int anzStellen(int zahl) {
 
+        int count = 0;
+        do {
+            zahl = zahl / 10;
+            count++;
+
+        } while (zahl > 0);
+        return count;
+    }
 
     // Bestimmt, ob eine als Parameter uebergebene
     // Zahl n (mit n > 0) eine narzistische Zahl ist
     // 3
+
+    public static boolean istNarzistisch(int zahl) {
+
+        int summe = 0;
+        int merkeZahl = zahl;
+
+        int anzahlStellen = anzStellen(zahl);
+
+        while (merkeZahl > 0) {
+            int zahl3 = merkeZahl % 10;
+            summe += Math.pow(zahl3, anzahlStellen);
+            merkeZahl /= 10;
+        }
+
+        if (summe == zahl) {
+            return true;
+            K
+        } else return false;
+    }
 
 
 
@@ -26,18 +53,55 @@ public class Klassenmethoden {
     // Es darf vorausgesetzt werden, das a,b > 0 gilt und a >= b
     // 3
 
+    public static int fehlendeZahl(int[] feld, int a, int b) {
 
+        for (int i = a; i <= b; i++) {
+            boolean vorhanden = false;
+            for (int zahl : feld) {
+                if (zahl == i) {
+                    vorhanden = true;
+                    break;
+                }
+            }
 
+            if (!vorhanden) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 
     /* Aufgabenteil (c) */
     /* Gibt ein Feld zurueck, welches die geraden Zahlen aus einem
        der Methode als Parameter uebergebenen Feld enthaelt */
     // 3
 
+    public static int[] filterGerade(int[] eingabeFeld) {
+        // Anzahl gerader Zahlen
+        int anzahlGerade = 0;
+        for (int zahl : eingabeFeld) {
+            if (zahl % 2 == 0) {
+                anzahlGerade++;
+            }
+        }
 
+
+        int[] geradeZahlen = new int[anzahlGerade];
+        int index = 0;
+
+        // Kopiere die geraden Zahlen in das neue Feld
+        for (int zahl : eingabeFeld) {
+            if (zahl % 2 == 0) {
+                geradeZahlen[index] = zahl;
+                index++;
+            }
+        }
+
+        return geradeZahlen;
+    }
 
     /* ********************************************************* */
-
 
     /* gibt den Inhalt des Felds a in der aktuellen Zeile aus
        und trennt Elemente mit einem ,
@@ -54,7 +118,7 @@ public class Klassenmethoden {
     }
 
     public static void main(String[] args) {
-        /*
+
         System.out.println("Aufgabenteil (a) ");
         for (int n : new int[] {112, 153, 1634, 1643, 54748, 55555, 548834}) {
             if (istNarzistisch(n)) {
@@ -62,9 +126,8 @@ public class Klassenmethoden {
                 System.out.println(anzStellen(n) + " Stellen");
             }
         }
-        */
 
-        /*
+
         System.out.println("\nAufgabenteil (b) ");
         int[][] felder = {{},
                           {1},
@@ -84,9 +147,8 @@ public class Klassenmethoden {
                 System.out.println(" fehlt keine Zahl");
             }
         }
-        */
 
-        /*
+
         System.out.println("\nAufgabenteil (c) ");
         int[][] felder2 = {{},
                            {1},
@@ -98,9 +160,7 @@ public class Klassenmethoden {
             printFeld(f2);
             System.out.println();
         }
-        */
 
         System.out.println();
     }
-
 }
