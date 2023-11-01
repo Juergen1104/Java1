b164374e667e2dafc1cd76000f34fdd75332e7fd
jpohl <jpohl@web.de>
2023-11-01 19:10:04 +0100

    Aufgabe 1

--- src/Semester_3/Aufgabe_7/StringTest.java
+++ src/Semester_3/Aufgabe_7/StringTest.java
@@ -4,23 +4,52 @@ public class StringTest {
 
   /* *** Aufgabe 1a *** */
   // 2
+
   public static String filterLetters(String s) {
 
-    return " "; // Dummy Return
+    StringBuffer stringBuffer = new StringBuffer(s.length());
+    int pos = 0;
+    for (int i = s.length() - 1; i > -1; i--) {
+
+      char c = s.charAt(i);
+      if (!Character.isLetter(c)) {
+        continue; // next character
+      }
+      stringBuffer.insert(pos, (Character.toLowerCase(c)));
+      pos++;
+    }
+    return stringBuffer.toString();
   }
 
   /* *** Aufgabe 1b *** */
   // 2.5
   public static boolean isPalindromeIt(String s) {
 
-    return false; // Dummy Return
+    int i = 0;
+    int j = s.length() - 1;
+    while (i < j) {
+      if (s.charAt(i) != s.charAt(j)) {
+        return false;
+      }
+      i++;
+      j--;
+    }
+    return true;
   }
 
   /* *** Aufgabe 1c *** */
   // 2.5
   public static boolean isPalindromeRek(String s) {
 
-    return false; // Dummy Return
+    if (s.length() <= 1) {
+      return true;
+    }
+
+    if (s.charAt(0) == s.charAt(s.length() - 1)) {
+      String innerStr = s.substring(1, s.length() - 1);
+      return isPalindromeRek(innerStr);
+    }
+    return false;
   }
 
   public static void main(String[] args) {
